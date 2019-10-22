f54617847b881d5ce668071e94871710466c0265
Jake Le <lejake888@gmail.com>
2019-10-22 11:05:45 +0100

    Attempted including DOM, unsure if needed

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
-let firstButton = () => {
-    document.getElementById("aboutMe").innerHTML = visible;}
+const button= document.getElementsByClassName("buttons");
+const boxes = document.getElementsByClassName("box")
+
+// for (i=0; i < boxes.length; i++)
+//     button.addEventListener("click", () => {
+
+//         if(boxes.style.visibility == "hidden") {
+//             boxes.style.visibility == "visible"
+//         } 
+//         else {
+//             boxes.style.visibility == "hidden";
+    
+//     }
+// });
